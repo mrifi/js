1b5f001b2ed2cd61d11711e637656d8d3c121e97
mrifi <mohaa.rifii@gmail.com>
2020-12-29 12:11:48 +0100

    operadores de todo tipo

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+/*Operadores matematicos
 let a = 5;
 let b = 2;
 
@@ -13,4 +14,39 @@ console.log(a/b);
 
 console.log(a%b);
 
-console.log("hola "+ "como estas " + usuario);
+console.log("hola "+ "como estas " + usuario);*/
+
+//operadores de incremento/decremento
+/*let a = 5;
+
+console.log(a); 
+// primero se muestra la 5 y despues el incremento
+console.log(a++);
+//en este caso la variable a ya vale 6 por lo tanto se imprimira un 6
+console.log(a); 
+//en este caso primero se suma y despues se muestra la variable que en este caso vale 7
+//basicamente esto sucede por el orden en el que escibimos el codigo si primero ponemas la variable, 
+//en ese momento la variable esta igual no cambia de valor  pero si despues se hace la suma "++" 
+//imprimimos la variable nos dara el resultado del incremento.
+console.log(++a);
+
+console.log(a--);
+console.log(a);
+console.log(--a);*/
+
+//operadores de asignacion
+
+// let a = 10;
+
+// // a= a+3;
+// a +=3;
+// console.log(a);
+// a -=3;
+// console.log(a);
+// a *=3;
+// console.log(a);
+// a %=3;
+// console.log(a); 
+
+
+
